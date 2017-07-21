7c9ac8c74fdf1eb978a689350a05365907392f69
Jukka Aho <jukka.aho@kapsi.fi>
2017-07-21 19:31:55 +0300

    automatically write runtests.jl if not found

--- src/PkgTestSuite.jl
+++ src/PkgTestSuite.jl
@@ -20,12 +20,16 @@ end
 if !haskey(installed_packages, "Lint")
     Pkg.add("Lint")
 end
+if !haskey(installed_packages, "TimerOutputs")
+    Pkg.add("TimerOutputs")
+end
 
 using CheckHeader
 using CheckTabs
 using Coverage
 using Documenter
 using Lint
+using TimerOutputs
 
 using Base.Test
 
@@ -107,6 +111,44 @@ function test(pkg::String="")
         strict = strict_docs)
 
     # run pkg tests
+    cd(Pkg.dir(pkg, "test"))
+    if !isfile("runtests.jl")
+        info("runtests.jl not found, creating default.")
+        runtests_code = """
+# This file is a part of JuliaFEM.
+# License is MIT: see https://github.com/JuliaFEM/$pkg.jl/blob/master/LICENSE
+
+using Base.Test
+using TimerOutputs
+
+pkg_dir = Pkg.dir("$pkg")
+maybe_test_files = readdir(joinpath(pkg_dir, "test"))
+is_test_file(fn) = startswith(fn, "test_") & endswith(fn, ".jl")
+test_files = filter(is_test_file, maybe_test_files)
+info("\$(length(test_files)) test files found.")
+
+const to = TimerOutput()
+@testset "$pkg.jl" begin
+    for fn in test_files
+        info("----- Running tests from file \$fn -----")
+        t0 = time()
+        timeit(to, fn) do
+            include(fn)
+        end
+        dt = round(time() - t0, 2)
+        info("----- Testing file \$fn completed in \$dt seconds -----")
+    end
+end
+println()
+println("Test statistics:")
+println(to)
+"""
+        println("writing the following runtests.jl file content:")
+        println(runtests_code)
+        fid = open("runtests.jl", "w")
+        write(fid, runtests_code)
+        close(fid)
+    end
     Pkg.test(pkg, coverage=true)
 end
 
